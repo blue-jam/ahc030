c1bb02bcf878da54549e7c7656ad9db3b2feed2a
blue_jam <blue-jam@users.noreply.github.com>
2024-02-10 11:59:19 +0900

    Simple solver + init: 20723000000

---
+++ ci.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import os
+import subprocess
+
+# Compile Kotlin code
+compile_command = "g++ -std=c++17 main.cpp"
+os.system(compile_command)
+
+# Initialize sum of outputs
+total_sum = 0
+
+run_command = f'seq 0 99 | xargs printf "%04d\n" | xargs -I@ -P8 sh -c "CI=true tools/target/debug/tester ./a.out < tools/in/@.txt > out/@.txt"'
+os.system(run_command)
+for i in range(0, 100):
+    input_file = f"tools/in/{i:04d}.txt"
+    output_file = f"out/{i:04d}.txt"
+
+    command = f'tools/target/debug/vis {input_file} {output_file}'
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    total_sum += int(stdout.decode('utf-8').split(' ')[2])
+
+# Print the total sum of outputs
+print(f"{total_sum}")
---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+struct P {
+    ll i, j;
+    P(ll i, ll j) : i(i), j(j) {}
+};
+
+void naive_solver(ll N, ll M, double e, vector<vector<P>> p) {
+    vector<P> result;
+    for (ll i = 0; i < N; i++) {
+        for (ll j = 0; j < N; j++) {
+            cout << "q 1 " << i << " " << j << endl;
+            flush(cout);
+            ll v;
+            cin >> v;
+            if (v > 0) {
+                result.emplace_back(i, j);
+            }
+        }
+    }
+
+    cout << "a " << result.size();
+    for (auto r: result) {
+        cout << " " << r.i << " " << r.j;
+    }
+    cout << endl;
+    flush(cout);
+    ll res;
+    cin >> res;
+}
+
+int main() {
+    ll N, M;
+    double e;
+    cin >> N >> M >> e;
+    vector<vector<P>> p(M);
+    for (ll i = 0; i < M; i++) {
+        ll d;
+        cin >> d;
+        for (ll j = 0; j < d; j++) {
+            ll a, b;
+            cin >> a >> b;
+            p[i].emplace_back(a, b);
+        }
+    }
+    naive_solver(N, M, e, p);
+
+    return 0;
+}
